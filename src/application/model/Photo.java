202a63e9753e41735d1f23f52bbe2e284a0d499f
Martin <martin.schutz@telecomnancy.net>
2014-11-06 16:17:56 +0100

    [ADD] Photo class in the model [ADD] MainInterface for the GUI and its controller MainInterfaceController

---
+++ src/application/model/Photo.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package application.model;
+
+/**
+ * A class that represent the photos in the data model
+ * @author Martin
+ *
+ */
+public class Photo {
+	
+	public Photo(){
+		
+	}
+
+}
---
+++ src/application/view/MainInterfaceController.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package application.view;
+
+/**
+ * A class that give the ids of the GUI components and implements their behaviors
+ * @author Martin
+ *
+ */
+public class MainInterfaceController {
+
+}
