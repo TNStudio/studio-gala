3098912279264dc591dd970ac6e406c4ce8f03f2
Martin <martin.schutz@telecomnancy.net>
2014-11-06 17:05:42 +0100

    [ADD] GUI roots containers

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,26 +1,34 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.AnchorPane;
 
-
+/**
+ * Main class of the project
+ * @author Martin
+ *
+ */
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
+			FXMLLoader loader = new FXMLLoader(); //create a loader to load the GUI
+            loader.setLocation(Main.class.getResource("view/MainInterface.fxml")); //find the GUI file
+			AnchorPane root = loader.load(); //load the GUI in a AnchorPane
+			Scene scene = new Scene(root); //create a scene with the GUI
+			primaryStage.setScene(scene); //put the scene in a stage (window)
+			primaryStage.setTitle("GALA printer Service by TN Studio"); //give a name to the window
+			primaryStage.setFullScreen(true); //set the window in fullscreen mode
+			primaryStage.show(); //display the window
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
 	}
 	
 	public static void main(String[] args) {
-		launch(args);
+		launch(args); //invoke start method
 	}
 }
