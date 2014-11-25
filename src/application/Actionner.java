22ff347395749d2739cd9f8aa7e6b43caefbee57
Martin <martin.schutz@telecomnancy.net>
2014-11-25 23:33:29 +0100

    [ADD] Actionner class which centralize all the actions todo by a ID

---
+++ src/application/Actionner.java
@@ -0,0 +1,19 @@
+package application;
+
+public class Actionner {
+	
+	private Main main;
+	
+	public static int REFRESH = 0;
+	public static int PARAMETERS = 1;
+	
+	
+	public Actionner(Main main){
+		this.main = main;
+	}
+	
+	public void doIt(int toDo){
+		
+	}
+
+}
