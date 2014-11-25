bb71d5d89eb8090d0bd4d37c48b8750220c64b44
Martin <martin.schutz@telecomnancy.net>
2014-11-25 23:53:33 +0100

    [ADD] validate print action

--- src/application/Actionner.java
+++ src/application/Actionner.java
@@ -1,17 +1,20 @@
 package application;
 
+import java.text.BreakIterator;
+
 public class Actionner {
-	
+
 	private Main main;
-	
+
 	public static final int REFRESH = 0;
 	public static final int SETTINGS = 1;
-	
-	
+	public static final int VALIDATE_PRINT = 2;
+
+
 	public Actionner(Main main){
 		this.main = main;
 	}
-	
+
 	public void doIt(int toDo){
 		switch (toDo) {
 		case REFRESH:
@@ -20,6 +23,10 @@ public class Actionner {
 
 		case SETTINGS:
 			main.loadInterface(main.getSettingsInterfaceController(), main.getSceneSettings());
+			break;
+
+		case VALIDATE_PRINT:
+			
 			break;
 		}
 	}
